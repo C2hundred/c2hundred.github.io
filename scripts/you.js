632364ae5728852dc95f276763d33f24eda428b9
C2hundred <101584061+C2hundred@users.noreply.github.com>
2022-08-28 11:09:27 +0100

    Update you.js

--- scripts/you.js
+++ scripts/you.js
@@ -1,39 +1,3 @@
-/* [Oct 2021] Added to comply with strict browser policies. */
-document.addEventListener('click', musicPlay);
-
-function musicPlay() {
-    var audio = document.getElementById('youare-audio');
-	var micon = document.getElementById('youare-micon');
-	
-	micon.addEventListener('click', musicPlay);
-	
-	if (audio.duration > 0 && audio.paused) {
-		audio.play();
-		micon.src = "https://youareanidiot.cc/images/speaker.png";
-	}
-	else {
-		audio.pause();
-		audio.currentTime = 0;
-		
-		micon.src = "https://youareanidiot.cc/images/speakerm.png";
-	}
-	
-	document.removeEventListener('click', musicPlay);
-}
-
-var faudio = new Audio('https://youareanidiot.cc/media/youare.mp3')
-
-faudio.addEventListener('timeupdate', function() {
-	console.log('TimeUpdate invoked.');
-
-    if (this.currentTime > this.duration - .45) {
-        this.currentTime = 0;
-        this.play();
-    }
-}
-);
-/* [Oct 2021] End part. */
-
 function bookmark() {
 	if ((navigator.appName == "Microsoft Internet Explorer") && (parseInt(navigator.appVersion) >= 4)) {
 		var url = "https://youareanidiot.cc/lol.html";
